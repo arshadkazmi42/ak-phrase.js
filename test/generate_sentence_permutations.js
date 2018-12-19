e61b1cdc0ad0ba772845b6ec2a1eb49dad3bb101
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-12-20 00:19:01 +0530

    tests added

---
+++ test/generate_sentence_permutations.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+
+const generator = require('../index');
+
+describe('Generating all permutation of sentences', () => {
+  it('should return all the generate phrases', async () => {
+    const wordList = [
+      ['eat'],
+      ['code', 'commit'],
+      ['sleep']
+    ]
+    const sentences = generator.generateSentences(wordList);
+    expect(sentences).to.deep.equal([ 'eat code sleep', 'eat commit sleep' ]);
+  });
+
+  it('should return empty list', async () => {
+    const wordList = []
+    const sentences = generator.generateSentences(wordList);
+    expect(sentences).to.equal(null);
+  });
+});
