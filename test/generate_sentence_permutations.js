5a303b00ab52ca835f9683e7d105b9cbeb0cca7f
cdrani <cdrani@users.noreply.github.com>
2019-03-28 22:39:34 -0600

    update generateSentences function import

--- test/generate_sentence_permutations.js
+++ test/generate_sentence_permutations.js
@@ -1,6 +1,6 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
 
-const generator = require('../index');
+const generateSentences = require('../index');
 
 describe('Generating all permutation of sentences', () => {
   it('should return all the generate phrases', async () => {
@@ -8,20 +8,20 @@ describe('Generating all permutation of sentences', () => {
       ['eat'],
       ['code', 'commit'],
       ['sleep']
-    ]
-    const sentences = generator.generateSentences(wordList);
+    ];
+    const sentences = generateSentences(wordList);
     expect(sentences).to.deep.equal([ 'eat code sleep', 'eat commit sleep' ]);
   });
 
   it('should return empty list', async () => {
     const wordList = []
-    const sentences = generator.generateSentences(wordList);
+    const sentences = generateSentences(wordList);
     expect(sentences).to.equal(null);
   });
 
   it('should return empty list for null input', async () => {
     const wordList = null
-    const sentences = generator.generateSentences(wordList);
+    const sentences = generateSentences(wordList);
     expect(sentences).to.equal(null);
   });
 });
